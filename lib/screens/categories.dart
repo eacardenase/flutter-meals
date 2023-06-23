7b11007b0229a218b20a0562acb92c16f1722eaa
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-22 19:27:55 -0500

    refactor: replace padding for slide transition on categories screen

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -85,9 +85,15 @@ class _CategoriesScreenState extends State<CategoriesScreen>
                 ))
             .toList(),
       ),
-      builder: (context, child) => Padding(
-        padding: EdgeInsets.only(
-          top: 100 - _animationController.value * 100,
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.25),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInOut,
+          ),
         ),
         child: child,
       ),
